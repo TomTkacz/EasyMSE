a1468653ba5473ae910650866afe7feed31c37cb
TomTkacz <antomationtv@gmail.com>
2023-09-24 14:59:08 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='ezmse',
+    version='0.0.1',
+    description='some package description',
+    author='Tom Tkacz',
+    author_email='thomasatkacz@gmail.com',
+    url='https://github.com/TomTkacz/EasyMSE',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'ezmse-cli = ezmse.main:main',
+        ]
+    }
+)
