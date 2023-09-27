4cc50aa2cbe579013ff92b7bf3c302900e42961f
TomTkacz <thomasatkacz@gmail.com>
2023-09-27 18:31:16 -0500

    improved package parsing for script sections

--- ezmse/package.py
+++ ezmse/package.py
@@ -11,18 +11,28 @@ class PackageHandler():
         result = {}
         keyStack = []
         lastIndent = -1
+        plainText = False
+        scriptIndent = -1
         
         lines = bytes.decode(packageData,encoding="utf-8-sig").rstrip().split('\n')
         
         for line in lines:
             
-            if line == '\n':
+            if line.strip() == '':
+                continue
+            
+            lineStripped = line.strip()
+            if lineStripped.startswith('#'):
                 continue
                         
             indent = line.count('\t')
-            if indent == lastIndent:
+            if indent == lastIndent and len(keyStack)>0 and not plainText:
                 keyStack.pop()
-                
+            
+            if indent <= scriptIndent:
+                plainText = False
+                scriptIndent = -1
+            
             if indent == 0:
                 currentDict = result
                 keyStack = []
@@ -35,24 +45,30 @@ class PackageHandler():
                     else:
                         break
                 currentDict = d
-            
-            lineStripped = line.strip()
-            if lineStripped.startswith('#'):
-                continue
 
-            keyValueList = lineStripped.split(': ', 1)
-            if len(keyValueList) == 1:
-                key = keyValueList[0][:-1]
-                value = None
-            elif len(keyValueList) == 2:
-                key,value = tuple(keyValueList)
+            if not plainText:
+                keyValueList = lineStripped.split(': ', 1)
+                if len(keyValueList) == 1:
+                    key = keyValueList[0][:-1]
+                    value = None
+                elif len(keyValueList) == 2:
+                    key,value = tuple(keyValueList)
                 
-            keyStack.append(key)
-                            
-            if not value:
-                currentDict.setdefault(key,{})
+                keyStack.append(key)
+                        
+                if not value:
+                    currentDict.setdefault(key,{})
+                else:
+                    currentDict.setdefault(key,value)
             else:
-                currentDict.setdefault(key,value)
+                try:
+                    currentDict['code'].append(lineStripped)
+                except:
+                    currentDict.setdefault('code',[lineStripped])
+                    
+            if 'script:' in line and not plainText:
+                plainText = True
+                scriptIndent = indent
 
             lastIndent = indent
                         
